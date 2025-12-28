3612e69ddaeea9e1267dac215ddd449a0424fc44
leonid9999999999 <leonidvishnevsky984@gmail.com>
2025-12-28 22:41:18 +0000

    Refactor App component and add Main

---
+++ my-app/src/client/components/header/Header.js
@@ -0,0 +1,22 @@
+import logo from '../../../resources/images/ima1logo.png';
+import './header.css';
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="Logo">
+        <img alt="logo" src={logo} />
+      </div>
+
+      <div className="header__nav">
+        <div className="header__link">Services</div>
+        <div className="header__link">Process</div>
+        <div className="header__link">Work</div>
+        <div className="header__button">Contact Us</div>
+      </div>
+
+    </div>
+  );
+}
+
+export default Header;
---
+++ my-app/src/client/components/main/main.js
@@ -0,0 +1,18 @@
+
+import './main.css';
+import Header from "../header/Header";
+
+
+function Main() {
+  return (
+    <div className="main">
+        
+        <Header/>
+         <Header/>
+          <Header/>
+           <Header/>
+    </div>
+  );
+}
+
+export default Main
